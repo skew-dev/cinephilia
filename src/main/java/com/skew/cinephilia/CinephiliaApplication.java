ecedfdf7a694a3b04ffca08e0805923a843e23fc
Rodrigo Castrejon <47370853+rcastrejon@users.noreply.github.com>
2021-10-13 15:55:32 -0500

    Initialize SpringBoot project

---
+++ src/main/java/com/skew/cinephilia/CinephiliaApplication.java
@@ -0,0 +1,13 @@
+package com.skew.cinephilia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinephiliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinephiliaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skew/cinephilia/CinephiliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skew.cinephilia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinephiliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

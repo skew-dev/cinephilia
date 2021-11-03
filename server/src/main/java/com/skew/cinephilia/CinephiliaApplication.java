0ca0f79dbbd501059fe01ae2cd39a531d25b3507
Antonio Cituk <56138035+Antonio-Cituk@users.noreply.github.com>
2021-11-03 13:15:45 -0500

    Update CinephiliaApplication.java

--- server/src/main/java/com/skew/cinephilia/CinephiliaApplication.java
+++ server/src/main/java/com/skew/cinephilia/CinephiliaApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CinephiliaApplication {
 
 	public static void main(String[] args) {
-		//This log should be deleted on next commit
 		System.out.println("Delete me!");
 		SpringApplication.run(CinephiliaApplication.class, args);
 	}

b5163291fb0added6d2246d6aa65e8d11e3d5201
Daniel-Pliego <dundingogog@outlook.com>
2021-11-20 19:44:58 -0600

    Added the first version of the website Also added new package.json

---
+++ web/src/components/CarouselImage.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let title;
+  export let shortDescription;
+  export let rating;
+</script>
+
+<div class="CarouselImage">
+  <div class="container">
+    <div class="title">
+      {title}
+    </div>
+    <div class="short-description">
+      {shortDescription}
+    </div>
+    <div class="rating">{rating}</div>
+  </div>
+</div>
+
+<style>
+  .container {
+    /* width: 100%; */
+    background-color: aqua;
+    padding: 5rem;
+  }
+</style>
---
+++ web/src/components/Search.svelte
@@ -0,0 +1,11 @@
+<script></script>
+
+<div class="Search">
+  <input type="text" placeholder="Buscar..." on:input />
+</div>
+
+<style>
+  input {
+    width: 500px;
+  }
+</style>

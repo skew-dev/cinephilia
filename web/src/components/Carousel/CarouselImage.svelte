5f270276f08701df589ab999eb817d06f482a54b
Daniel-Pliego <dundingogog@outlook.com>
2021-12-09 13:44:46 -0600

    -Added provisional images -Organized files

---
+++ web/src/components/Carousel/CarouselImage.svelte
@@ -0,0 +1,59 @@
+<!-- TEMPORAL -->
+<script>
+  export let imgUrl;
+  export let title;
+  export let shortDescription;
+  export let rating;
+
+  import Stars from "./Stars/Stars.svelte";
+</script>
+
+<div class=" container content is-medium">
+  <img src={imgUrl} />
+  <div class="Content">
+    <div class="title">
+      <h4>{title}</h4>
+    </div>
+    <div class="shortDescription">
+      <h5>
+        {shortDescription}
+      </h5>
+    </div>
+  </div>
+  <div class="rating">
+    <Stars {rating} />
+  </div>
+</div>
+
+<style>
+  .container {
+    position: relative;
+    display: inline-block;
+    text-align: center;
+  }
+
+  img {
+    width: 65%;
+    height: auto;
+    object-fit: cover;
+  }
+
+  .Content {
+    position: absolute;
+    top: 70%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  h4,
+  h5 {
+    margin: 0;
+    padding: 5px 0;
+    color: rgb(250, 250, 248);
+    text-shadow: 2px 2px rgb(0, 0, 0);
+  }
+
+  .title {
+    margin-bottom: 0.5rem;
+  }
+</style>
